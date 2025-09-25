817e1db4afed93f9dce2aeba172043b96468a564
Christian Rafael <christianparadix@gmail.com>
2025-09-24 23:02:38 -0300

    Largest numbers

---
+++ algorithms/largest-of-array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        if (a[i] > largest) {
+            largest = a[i];
+        }
+
+        if (a[i] < smallest) {
+            smallest = a[i];
+        }
+    }
+
+    cout << "largest value = " << largest << endl;
+    cout << "smallest value = " << smallest << endl;
+
+    return 0;
+}
